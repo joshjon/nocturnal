6dde9ddd988e6624b8267755d2ef66350b60bc74
Joshua Jon <joshua.j.jon@gmail.com>
2019-12-09 21:13:39 +1100

    Custom time window (#4)

---
+++ Nocturnal/Application/Custom Time/CustomTimeWindowController.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTimeWindowController.swift
+//  Nocturnal
+//
+//  Created by Joshua Jon on 9/12/19.
+//  Copyright © 2019 Joshua Jon. All rights reserved.
+//
+
+import Cocoa
+
+class CustomTimeWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        guard let window = self.window else { return }
+        window.level = .floating
+        window.center()
+    }
+
+}
