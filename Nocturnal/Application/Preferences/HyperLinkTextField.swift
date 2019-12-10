1818b9b31164486b857ee46e5d21ec59917e1ae6
JoshJon <joshua.j.jon@gmail.com>
2019-12-10 17:24:00 +1100

    Add hyperlink text field

---
+++ Nocturnal/Application/Preferences/HyperLinkTextField.swift
@@ -0,0 +1,38 @@
+//
+//  HyperLinkTextField.swift
+//  Nocturnal
+//
+//  Created by Joshua Jon on 10/12/19.
+//  Copyright © 2019 Joshua Jon. All rights reserved.
+//
+
+import Cocoa
+
+@IBDesignable
+class HyperlinkTextField: NSTextField {
+
+    @IBInspectable var href: String = ""
+
+    override func resetCursorRects() {
+        discardCursorRects()
+        addCursorRect(self.bounds, cursor: NSCursor.pointingHand)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        // TODO:  Fix this and get the hover click to work.
+
+        let attributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.foregroundColor: NSColor.linkColor,
+            NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue as AnyObject
+        ]
+        attributedStringValue = NSAttributedString(string: self.stringValue, attributes: attributes)
+    }
+
+    override func mouseDown(with theEvent: NSEvent) {
+        if let localHref = URL(string: href) {
+            NSWorkspace.shared.open(localHref)
+        }
+    }
+}
