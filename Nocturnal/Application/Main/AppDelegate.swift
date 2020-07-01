e3c7489a84101d6fdb2a2ced86e08ba98bf160c0
JoshJon <joshua.j.jon@gmail.com>
2020-07-01 18:01:28 +1000

    Reformat

--- Nocturnal/Application/Main/AppDelegate.swift
+++ Nocturnal/Application/Main/AppDelegate.swift
@@ -11,14 +11,14 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     static var dimnessControllers = [] as [DimnessWindowController]
-    
+
     func applicationDidFinishLaunching(_: Notification) {
         initDimnessControllers()
         NightShift.blueLightReductionAmount = 0
         NightShift.enable()
         addNotificationObservers()
     }
-    
+
     func initDimnessControllers() {
         for i in 0 ..< NSScreen.screens.count {
             let dimnessWindow = DimnessWindowController(screen: NSScreen.screens[i])
@@ -26,22 +26,22 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             dimnessWindow.showWindow(self)
         }
     }
-    
+
     func addNotificationObservers() {
         NotificationCenter.default.addObserver(self, selector: #selector(onNumberOfScreensChanged),
                                                name: NSApplication.didChangeScreenParametersNotification, object: nil)
-        
+
         DistributedNotificationCenter.default.addObserver(self, selector: #selector(onUnlock),
                                                           name: .init("com.apple.screenIsUnlocked"), object: nil)
     }
-    
+
     @objc func onNumberOfScreensChanged() {
         AppDelegate.dimnessControllers.removeAll()
         initDimnessControllers()
     }
-    
+
     @objc func onUnlock() {
-        if (StateManager.isTouchBarHidden) {
+        if StateManager.isTouchBarHidden {
             do { sleep(1) } // wait for TouchBar init
             TouchBarController.shared.hideTouchBar()
         }
