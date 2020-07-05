b832ef6e14be40ff96cf3b3aa99deb304413b7ad
JoshJon <joshua.j.jon@gmail.com>
2020-07-05 14:30:48 +1000

    Remove pods

--- Podfile
+++
@@ -1,6 +0,0 @@
-platform :osx, '10.15'
-
-target 'Nocturnal' do
-  use_frameworks!
-  pod 'MASShortcut'
-end
--- Pods/Target Support Files/Pods-Nocturnal/Pods-Nocturnal-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Nocturnal : NSObject
-@end
-@implementation PodsDummy_Pods_Nocturnal
-@end
